a4263f92158dafcb15b7a59620f784ac20123666
Rakibjrb <rakibjrb572157@gmail.com>
2024-04-22 09:19:09 +0600

    completed header section

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,7 +1,24 @@
-import React from "react";
+import HeaderLogo from "@/assets/the-dragon-news.png";
+import moment from "moment";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="text-center mt-4 px-5 md:px-0 space-y-2">
+      <Image
+        className="mx-auto"
+        src={HeaderLogo}
+        width={500}
+        height={200}
+        alt="header logo"
+        placeholder="blur"
+        priority={true}
+      />
+
+      <h1 className="text-xl md:text-2xl">Journalism Without Fear or Favour</h1>
+      <h2 className="text-xl">{moment().format("llll")}</h2>
+    </div>
+  );
 };
 
 export default Header;
