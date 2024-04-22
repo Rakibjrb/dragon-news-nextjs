5cdb16a90b7d3d7f3f52a9acdfb0d31d6ac63fcb
Rakibjrb <rakibjrb572157@gmail.com>
2024-04-22 23:36:34 +0600

    fixed data fetching problem on category search

--- src/app/api/news/category/[category]/route.js
+++ src/app/api/news/category/[category]/route.js
@@ -3,18 +3,12 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
   try {
-    const queryValue = req.url.split("?")[1].split("=")[1];
-
-    if (queryValue === "All") {
+    const category = req.url.split("category/")[1];
+    if (category === "All") {
       const newses = await newsCollection.find({}).toArray();
       return NextResponse.json({ data: newses });
     }
-
-    const query = {
-      category: queryValue,
-    };
-
-    const newses = await newsCollection.find(query).toArray();
+    const newses = await newsCollection.find({ category }).toArray();
     return NextResponse.json({ data: newses });
   } catch (error) {
     return NextResponse.json({ message: "something went wrong", error });
